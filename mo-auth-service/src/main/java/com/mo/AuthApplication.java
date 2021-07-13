db5a975652bed7662248bf8f130c6650aa4acb25
moweiquan <295597613@qq.com>
2021-07-13 19:50:47 +0800

    注册登录服务平台-auth 认证服务平台

---
+++ mo-auth-service/src/main/java/com/mo/AuthApplication.java
@@ -0,0 +1,19 @@
+package com.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Created by mo on 2021/7/13
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
---
+++ mo-common/src/main/java/com/mo/config/MybatisPlusPageConfig.java
@@ -0,0 +1,28 @@
+package com.mo.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by mo on 2021/7/13
+ */
+@Configuration
+public class MybatisPlusPageConfig {
+
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则
+     * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
+     * @return
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+
+    }
+}
