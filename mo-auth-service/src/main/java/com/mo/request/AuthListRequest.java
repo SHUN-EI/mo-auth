790ec9ff0081fd755c8d4b82351ab07bc14d0c39
moweiquan <295597613@qq.com>
2021-07-15 20:29:41 +0800

    注册登录服务平台-查询用户认证信息接口-分页带条件查询接口

--- mo-auth-service/src/main/java/com/mo/request/AuthListRequest.java
+++ mo-auth-service/src/main/java/com/mo/request/AuthListRequest.java
@@ -2,6 +2,8 @@ package com.mo.request;
 
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * Created by mo on 2021/7/15
  */
@@ -16,4 +18,82 @@ public class AuthListRequest {
      * 每页显示多少条
      */
     private Integer pageSize;
+
+    private String id;
+
+    /**
+     * 用户账号
+     */
+    private String userName;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 账户状态，1正常，0测试，-1禁用
+     */
+    private Integer status;
+
+
+    /**
+     * 用户手机号
+     */
+    private String mobile;
+
+    /**
+     * 绑定手机的时间
+     */
+    private Date mobileBindDate;
+
+    /**
+     * 绑定邮箱
+     */
+    private String email;
+
+    /**
+     * 绑定邮箱的时间
+     */
+    private Date emailBindDate;
+
+    /**
+     * 绑定QQ
+     */
+    private String qq;
+
+    /**
+     * 绑定QQ的时间
+     */
+    private Date qqBindDate;
+
+    /**
+     * 绑定微信唯一标识符
+     */
+    private String weixin;
+
+    /**
+     * 绑定微信的时间
+     */
+    private Date weixinBindDate;
+
+    /**
+     * 绑定新浪微博唯一标识符
+     */
+    private String weibo;
+
+    /**
+     * 绑定新浪微博的时间
+     */
+    private Date weiboBindDate;
+
+    /**
+     * 用户注册的时间
+     */
+    private Date createDate;
+
+    /**
+     * 用户最后一次登录的时间
+     */
+    private Date lastDate;
 }
--- mo-auth-service/src/main/java/com/mo/service/impl/AuthServiceImpl.java
+++ mo-auth-service/src/main/java/com/mo/service/impl/AuthServiceImpl.java
@@ -30,6 +30,25 @@ public class AuthServiceImpl extends ServiceImpl<AuthMapper, Auth> implements Au
         QueryWrapper<Auth> wrapper = new QueryWrapper<>();
         wrapper.orderByDesc("last_date");
 
+
+        //封装查询条件，不为null作为查询条件
+        wrapper.eq(request.getId() != null, "id", request.getId());
+        wrapper.eq(request.getUserName() != null, "user_name", request.getUserName());
+        wrapper.eq(request.getMobile() != null, "mobile", request.getMobile());
+        wrapper.eq(request.getEmail() != null, "email", request.getEmail());
+        wrapper.eq(request.getWeixin() != null, "weixin", request.getWeixin());
+        wrapper.eq(request.getWeibo() != null, "weibo", request.getWeibo());
+        wrapper.eq(request.getQq() != null, "qq", request.getQq());
+        wrapper.eq(request.getStatus() != null, "status", request.getStatus());
+
+        wrapper.ge(request.getMobileBindDate() != null, "mobile_bind_date", request.getMobileBindDate());
+        wrapper.ge(request.getEmailBindDate() != null, "email_bind_date", request.getEmailBindDate());
+        wrapper.ge(request.getWeiboBindDate() != null, "weixin_bind_date", request.getWeiboBindDate());
+        wrapper.ge(request.getWeiboBindDate() != null, "weibo_bind_date", request.getWeiboBindDate());
+        wrapper.ge(request.getQqBindDate() != null, "qq_bind_date", request.getQqBindDate());
+        wrapper.ge(request.getCreateDate() != null, "create_date", request.getCreateDate());
+        wrapper.ge(request.getLastDate() != null, "last_date", request.getLastDate());
+
         page = page(page, wrapper);
         List<Auth> authList = page.getRecords();
         List<AuthDTO> authDTOList = authList.stream().map(obj -> {
