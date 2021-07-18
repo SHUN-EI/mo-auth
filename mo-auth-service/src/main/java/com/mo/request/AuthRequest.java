85ecdf844138fd080a27b25f26c440979edfe435
moweiquan <295597613@qq.com>
2021-07-19 01:35:23 +0800

    注册登录服务平台-引入spring validator参数检验，异常处理器添加参数校验异常的处理

--- mo-auth-service/src/main/java/com/mo/request/AuthRequest.java
+++ mo-auth-service/src/main/java/com/mo/request/AuthRequest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.checkerframework.common.value.qual.MinLen;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Date;
 
--- mo-common/src/main/java/com/mo/exception/ExceptionHandle.java
+++ mo-common/src/main/java/com/mo/exception/ExceptionHandle.java
@@ -1,10 +1,15 @@
 package com.mo.exception;
 
 import com.mo.model.Result;
+import com.mo.model.ResultCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.Set;
 
 /**
  * Created by mo on 2021/7/18
@@ -17,15 +22,24 @@ public class ExceptionHandle {
     @ResponseBody
     public Result handle(Exception e) {
 
+        StringBuffer sb = new StringBuffer();
+
         //是否为自定义异常
         if (e instanceof BizException) {
             BizException bizException = (BizException) e;
             log.info("[业务异常]{}", e);
             //返回前端的错误信息
             return Result.error(bizException.getCode(), bizException.getMsg());
+        } else if (e instanceof ConstraintViolationException) {
+            //参数检验异常
+            Set<ConstraintViolation<?>> constraintViolations = ((ConstraintViolationException) e).getConstraintViolations();
+            constraintViolations.forEach(msg -> sb.append(msg.getMessage()).append(";"));
+            log.info("[参数检验异常]{}", e);
+            return Result.error(ResultCode.BAD_REQUEST, sb.toString());
         } else {
             log.info("[系统异常]{}", e);
             return Result.error("全局异常，未知错误");
         }
     }
+
 }
