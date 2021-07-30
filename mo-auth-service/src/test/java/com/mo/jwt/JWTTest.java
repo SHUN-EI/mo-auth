0535fe0a9a9e02c4aaf39bdd23add300dc4ea285
moweiquan <295597613@qq.com>
2021-07-30 20:31:25 +0800

    注册登录服务平台-jwt过期时间测试

--- mo-auth-service/src/test/java/com/mo/jwt/JWTTest.java
+++ mo-auth-service/src/test/java/com/mo/jwt/JWTTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.sql.Date;
 import java.util.Base64;
 
 /**
@@ -26,9 +27,13 @@ public class JWTTest {
     //声明盐，不能泄露，存放在服务器
     private static String secret = "TQB^Ipw7KxMPj2nPx0FbRD%$M";
 
+    //token过期时间，1小时
+    private static final long EXPIRED = 1000 * 60 * 60;
+
     @Test
     public void parserJWTTest() {
-        String jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJtby1hdXRoIn0.9_6Xmq_yg6q4d7g78rjm5FxqApUKirVo4iIVwxJ2eeA";
+        // String jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJtby1hdXRoIn0.9_6Xmq_yg6q4d7g78rjm5FxqApUKirVo4iIVwxJ2eeA";
+        String jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJtby1hdXRoIiwiaWF0IjoxNjI3NjQ4MjMwLCJleHAiOjE2Mjc2NTE4MzB9.wjvDfvI07pEappdsyxDjIfC8QKOIyuNrp8aY8Y0FVOY";
 
         //生成签名的key,使用java自带的Base64加密
         byte[] encodeKey = Base64.getEncoder().encode(secret.getBytes());
@@ -41,6 +46,8 @@ public class JWTTest {
                 .getBody();
 
         log.info("jwt token的 id 为:{}", claims.getId());
+        log.info("jwt token的 签发时间为:{}", claims.getIssuedAt());
+        log.info("jwt token的 过期时间为:{}", claims.getExpiration());
 
     }
 
@@ -57,6 +64,9 @@ public class JWTTest {
                 .setHeaderParam("typ", "JWT")
                 .setHeaderParam("alg", "HS256")
                 .setId("mo-auth")//声明唯一标识jti
+                //设置时间
+                .setIssuedAt(new Date(System.currentTimeMillis()))//设置jwt签发时间
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRED))//设置jwt过期时间
                 .signWith(SignatureAlgorithm.HS256, secretKey); //设置密钥签名
 
         log.info("生成的jwt token为:{}", jwtBuilder.compact());
