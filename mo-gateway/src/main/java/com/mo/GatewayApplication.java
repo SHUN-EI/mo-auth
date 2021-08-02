b65f29c92ee1210a2578259a1a04bf4593457428
moweiquan <295597613@qq.com>
2021-08-02 19:43:19 +0800

    注册登录服务平台-引入springcloud-gateway，做网关转发

---
+++ mo-gateway/src/main/java/com/mo/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by mo on 2021/8/2
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
