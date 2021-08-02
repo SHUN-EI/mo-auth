a439d5ee78b8da309f3ffa2db4b6d997bcf02e45
moweiquan <295597613@qq.com>
2021-08-02 21:32:24 +0800

    注册登录服务平台-解决springcloud-gateway与springboot web的包冲突问题

--- mo-gateway/src/main/java/com/mo/GatewayApplication.java
+++ mo-gateway/src/main/java/com/mo/GatewayApplication.java
@@ -2,12 +2,13 @@ package com.mo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by mo on 2021/8/2
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
