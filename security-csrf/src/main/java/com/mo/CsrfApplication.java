3d0042b9d3fd39b9bdf5b2e4392c39d4fd488f0d
moweiquan <295597613@qq.com>
2021-08-18 18:57:10 +0800

    注册登录服务平台-模拟csrf攻击

---
+++ security-csrf/src/main/java/com/mo/CsrfApplication.java
@@ -0,0 +1,11 @@
+package com.mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrfApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CsrfApplication.class, args);
+    }
+}
