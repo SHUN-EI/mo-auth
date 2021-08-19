f4b03b14a62619e88eb72bf08a2b493eef32054a
moweiquan <295597613@qq.com>
2021-08-19 20:32:23 +0800

    注册登录服务平台-安全项目，防止XSS攻击

--- security-test/src/main/java/com/mo/SecurityApplication.java
+++ security-test/src/main/java/com/mo/SecurityApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Created by mo on 2021/8/13
+ * Created by mo on 2021/8/19
  */
 @SpringBootApplication
 public class SecurityApplication {
